8591b6c4ca8a70802ce74ca90f5edef8c889b589
zxl20070701 <1904314465@qq.com>
2025-12-24 21:13:05 +0800

    添加env并优化proxy等配置方式

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,38 @@
-import { defineConfig } from "vite";
+import { loadEnv, ConfigEnv } from "vite";
 import uni from "@dcloudio/vite-plugin-uni";
+import { resolve } from "path";
+import { createHtmlPlugin } from "vite-plugin-html";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  server: {
-    host: "0.0.0.0",
-    port: 8080,
-  },
-  plugins: [uni()],
-});
+export default ({ mode }: ConfigEnv): any => {
+  const env = loadEnv(mode, process.cwd());
+  return {
+    resolve: {
+      alias: {
+        "@": resolve(__dirname, "./src")
+      },
+    },
+    server: {
+      host: "0.0.0.0",
+      port: 8080,
+      proxy: (() => {
+        let proxy: any = {};
+        proxy[env.VITE_APP_PROXY_PRE] = {
+          target: env.VITE_APP_PROXY_URL,
+          changeOrigin: true
+        };
+        return proxy;
+      })()
+    },
+    plugins: [
+      uni(),
+      createHtmlPlugin({
+        inject: {
+          data: {
+            staticUrl: env.VITE_APP_STATIC_URL
+          }
+        },
+      })
+    ]
+  };
+};
